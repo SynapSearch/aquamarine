b7f8dbb151b87254148f8930ea394a2c4d1b803f
Lucy Zhang <zhangly@mit.edu>
2021-02-19 23:03:12 -0800

    initial implementation

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,18 @@ class UserSignupForm(UserCreationForm):
 		model = UserProfile
 		fields = ['username','password1','password2','is_recruiter']
 
-	username = forms.EmailField(max_length=255, error_messages={'required': 'Please enter a valid email address.'})
+	username = forms.EmailField(max_length=255, 
+		widget=forms.TextInput(attrs={'placeholder':'Email'}),
+		error_messages={'required': 'Please enter a valid email address.'})
+	password1 = forms.CharField(max_length=255,
+		widget=forms.PasswordInput(attrs={'placeholder':'Password'}),
+		error_messages={'required': 'Passwords must be at least 8 characters long.'})
+	password2 = forms.CharField(max_length=255,
+		widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}),
+		error_messages={'required': 'Passwords must match.'})
+	is_recruiter= forms.CharField(label='Account type', widget=forms.Select(
+		choices=[(False, "Job seeker"), (True, "Employer")]))
+
 
 class RecruiterProfileForm(forms.ModelForm):
 	class Meta:
