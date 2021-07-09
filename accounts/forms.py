f9ab391491b7a01c6cfd2482e3d253b0671867d1
HP34 <hrithasmapant@gmail.com>
2021-07-08 20:20:09 -0400

    added error message to StudentProfileForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import UserProfile, RecruiterProfile, StudentProfile, Experience, Project, Involvement
 from django.db import models, transaction
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 
 class UserSignupForm(UserCreationForm):
 	class Meta:
@@ -40,6 +41,23 @@ class StudentProfileForm(forms.ModelForm):
 	class Meta:
 		model = StudentProfile
 		fields = ['first_name', 'last_name', 'school', 'graduation_year', 'major', 'picture']
+
+	first_name = forms.CharField(max_length=13, validators=[MinLengthValidator(2)],
+        widget=forms.TextInput(attrs={'placeholder': 'First Name'}),
+        error_messages={'required': 'First name must be at least 2 characters but no more than 13 characters'})
+	last_name = forms.CharField(max_length=13, validators=[MinLengthValidator(2)],
+		widget=forms.TextInput(attrs={'placeholder': 'Last Name'}),
+		error_messages={'required': 'Last name must be at least 2 characters but no more than 13 characters'})
+	school = forms.CharField(max_length=100, validators=[MinLengthValidator(2)], 
+		widget=forms.TextInput(attrs={'placeholder': 'School'}),
+		error_messages={'required': 'School must not exceed 100 characters'})
+	graduation_year = forms.IntegerField(validators=[MinValueValidator(2010), MaxValueValidator(2030)],
+		widget=forms.TextInput(attrs={'placeholder': 'Graduation Year'}),
+		error_messages={'required': 'Graduation Year entered must be between 2010 to 2030'})
+	major = forms.CharField(max_length=100,
+		widget=forms.TextInput(attrs={'placeholder': 'Major'}),
+		error_messages={'required': 'Major must not exceed 100 characters'})
+    # add picture?
 
 class ExperienceForm(forms.ModelForm):
 	class Meta: 
